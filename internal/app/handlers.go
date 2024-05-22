452454dc378dccc3479ffeacf29d95efec838315
Amr M. Elsayed <rixtrayker@github.com>
2024-05-22 11:28:30 +0300

    chore: Refactor handlers.go to improve code structure and readability

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -1,20 +1,67 @@
 package app
 
 import (
-	"github.com/rixtrayker/demo-smpp/internal/smpp"
+	"fmt"
+	"log"
+
+	"github.com/linxGnu/gosmpp/pdu"
 )
 
-func HandleProviderA(smppClient *smpp.Client, message string) error {
-    // handle sending message for provider A
-    return smppClient.SendMessage("A", message)
+func ProviderAHandler(func(pdu.PDU)) func(pdu.PDU, bool) {
+    // Handler logic for provider A
+    return handlePDU()
+} 
+
+func ProviderBHandler(func(pdu.PDU)) func(pdu.PDU, bool) {
+    // Handler logic for provider B
+    return handlePDU()
+
 }
 
-func HandleProviderB(smppClient *smpp.Client, message string) error {
-    // handle sending message for provider B
-    return smppClient.SendMessage("B", message)
+func ProviderCHandler(func(pdu.PDU)) func(pdu.PDU, bool) {
+    // Handler logic for provider C
+    return handlePDU()
 }
 
-func HandleProviderC(smppClient *smpp.Client, message string) error {
-    // handle sending message for provider C
-    return smppClient.SendMessage("C", message)
+
+func handlePDU() func(pdu.PDU, bool) {
+	concatenated := map[uint8][]string{}
+	return func(p pdu.PDU, _ bool) {
+		switch pd := p.(type) {
+		case *pdu.SubmitSMResp:
+			fmt.Println("SubmitSMResp:")//pd)
+
+		case *pdu.GenericNack:
+			// fmt.Println("GenericNack Received")
+
+		case *pdu.EnquireLinkResp:
+			// fmt.Println("EnquireLinkResp Received")
+
+		case *pdu.DataSM:
+			// fmt.Printf("DataSM:")//pd)
+
+		case *pdu.DeliverSM:
+			// fmt.Printf("DeliverSM:")//pd)
+			// log.Println(pd.Message.GetMessage())
+			// region concatenated sms (sample code)
+			message, err := pd.Message.GetMessage()
+			if err != nil {
+				log.Fatal(err)
+			}
+			totalParts, sequence, reference, found := pd.Message.UDH().GetConcatInfo()
+			if found {
+				if _, ok := concatenated[reference]; !ok {
+					concatenated[reference] = make([]string, totalParts)
+				}
+				concatenated[reference][sequence-1] = message
+			}
+			if !found {
+				// log.Println(message)
+			} else if _, ok := concatenated[reference]; ok {
+                log.Println(concatenated[reference])
+				// delete(concatenated, reference)
+			}
+			// endregion
+		}
+	}
 }
