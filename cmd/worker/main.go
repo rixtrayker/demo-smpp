e41c100daa2c761206b1f38286faa751d6d20bea
Amr M. Elsayed <rixtrayker@github.com>
2024-05-22 11:28:18 +0300

    Refactor main.go to improve code organization and readability

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"github.com/rixtrayker/demo-smpp/internal/config"
-    "github.com/rixtrayker/demo-smpp/internal/db"
-    "github.com/rixtrayker/demo-smpp/internal/smpp"
-    "github.com/rixtrayker/demo-smpp/internal/app"
 	"github.com/joho/godotenv"
+	"github.com/rixtrayker/demo-smpp/internal/app"
+	"github.com/rixtrayker/demo-smpp/internal/config"
 	"github.com/sirupsen/logrus"
 )
 
@@ -16,15 +14,6 @@ func main() {
     }
 
     cfg := config.LoadConfig()
-    db := db.Init(cfg)
 
-    providerSessions := map[string]*smpp.Session{
-        "A": smpp.NewSession(cfg, "A"),
-        "B": smpp.NewSession(cfg, "B"),
-        "C": smpp.NewSession(cfg, "C"),
-    }
-
-    smppClient := smpp.NewSession(providerSessions, cfg)
-
-    app.StartWorker(db, smppClient, cfg)
+    app.StartWorker(&cfg)
 }
