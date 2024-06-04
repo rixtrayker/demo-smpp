963341c4fde7c7bd4fbbe1375a5d3bc9e1c582b0
Amr M. Elsayed <rixtrayker@github.com>
2024-06-04 05:37:19 +0300

    chore: Refactor session code to handle different session types and improve error handling

--- internal/session/send.go
+++ internal/session/send.go
@@ -1,23 +1,38 @@
 package session
 
 import (
+	"strconv"
+
 	"github.com/linxGnu/gosmpp/data"
 	"github.com/linxGnu/gosmpp/pdu"
+	"github.com/rixtrayker/demo-smpp/internal/dtos"
+	"github.com/rixtrayker/demo-smpp/internal/queue"
 	"github.com/sirupsen/logrus"
 )
 
-func (s *Session) Send(sender, number, message string) error {
-    submitSM := newSubmitSM(sender, number, message)
-    logrus.Info("Sending message to ", number)
+// ported redirect: zain -> mobily -> stc
+var gateways = []string{"zain", "mobily", "stc"}
+
+func (s *Session) Send(msg queue.MessageData) error {
+    submitSM := newSubmitSM(msg.Sender, msg.Number, msg.Text)
+    logrus.Info("Sending message to ", msg.Number)
     ref := submitSM.SequenceNumber
+
+    gh := append(msg.GatewayHistory, s.gateway)
+
     s.mu.Lock()
-    s.Status[ref] = &MessageStatus{
-        Number: number,
+    s.MessagesStatus[ref] = &MessageStatus{
+        SystemMessageID: msg.Id,
+        Sender:          msg.Sender,
+        Text:            msg.Text,
+        Number:          msg.Number,
+        GatewayHistory:  gh,
     }
+
     s.mu.Unlock()
 
     if s.hasOutstanding {
-        s.outstandingCh <- struct{}{}
+        s.OutstandingCh <- struct{}{}
         return s.send(submitSM)
     } else {
         return s.send(submitSM)
@@ -30,6 +45,70 @@ func (s *Session) send(submitSM *pdu.SubmitSM) error{
     } else {
         return s.transmitter.Transmitter().Submit(submitSM)
     }
+}
+
+func (s *Session) handleSubmitSMResp(pd *pdu.SubmitSMResp) (pdu.PDU, bool) {
+    select {
+    case <-s.OutstandingCh:
+        return s.processSubmitSMResp(pd)
+    default:
+        return s.processSubmitSMResp(pd)
+    }
+}
+
+func (s *Session) processSubmitSMResp(pd *pdu.SubmitSMResp) (pdu.PDU, bool) {
+    ref := pd.SequenceNumber
+    s.mu.Lock()
+    messageStatus := s.MessagesStatus[ref]
+    s.mu.Unlock()
+
+    errCode := strconv.Itoa(int(pd.CommandStatus))
+    id := strconv.Itoa(messageStatus.SystemMessageID)
+
+    s.responseWriter.WriteResponse(&dtos.ReceiveLog{
+        MessageID:    id,
+        Gateway:      s.gateway,
+        MobileNo:     messageStatus.Number,
+        MessageState: "Sent",
+        ErrorCode:    errCode,
+    })
+
+    if(pd.CommandStatus == 0) {
+        logrus.Info("SubmitSMResp Received")
+    } else {
+        if pd.CommandStatus == data.ESME_RINVDSTADR || s.gateway == "stc" {
+            go s.portMessage(messageStatus)
+        }
+    }
+
+    logrus.Info("SubmitSMResp Received")
+    return pd.GetResponse(), false
+}
+
+
+func (s *Session) portMessage(messageStatus *MessageStatus) {    
+    s.ResendChannel <- queue.MessageData{
+        Id: messageStatus.SystemMessageID,
+        Gateway: s.portGateway(messageStatus.GatewayHistory),
+        Sender: messageStatus.Sender,
+        Text: messageStatus.Text,
+        Number: messageStatus.Number,
+        GatewayHistory: messageStatus.GatewayHistory,
+    }
+}
+
+// todo: history not including the result !!!!
+func (s *Session) portGateway(history []string) string {
+    // ["zain", "mobily", "stc"]
+    if len(history) == 1 {
+        return gateways[1]
+    }
+
+    if len(history) == 2 {
+        return gateways[2]
+    }
+
+    return "Mobily"
 }
 
 func newSubmitSM(sender, number, message string) *pdu.SubmitSM {
