10d21c2535bc067dd7d165df86f52261acf728fe
Amr M. Elsayed <rixtrayker@github.com>
2024-06-08 03:01:22 +0300

    Refactor configuration file to separate provider settings and database settings

--- go.mod
+++ go.mod
@@ -21,13 +21,30 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.19.0 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240514024235-59d9797072e7 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,153 +1,109 @@
 package config
 
 import (
-	"os"
-	"strconv"
-	"strings"
+	"log"
+
+	"github.com/spf13/viper"
 )
 
-var config *Config
-var providersList []Provider
 type Config struct {
-    RedisURL         string
-    RateLimit        int
-    SMPPConfig       SMPPConfig
-    DatabaseConfig   DatabaseConfig
-    ProvidersConfig  []Provider
+	RedisURL        string
+	RateLimit       int
+	SMPPConfig      SMPPConfig
+	DatabaseConfig  DatabaseConfig
+	ProvidersConfig []Provider
 }
 
 type SMPPConfig struct {
-    SMSC       string
-    SystemID   string
-    Password   string
-    SystemType string
+	SMSC       string
+	SystemID   string
+	Password   string
+	SystemType string
 }
 
 type DatabaseConfig struct {
-    Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
-	DBName   string `yaml:"dbname"`
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
 }
 
 type Provider struct {
-    Name            string
-    SessionType     string
-    SMSC            string
-    SystemID        string
-    Password        string
-    SystemType      string
-    MaxOutStanding  int
-    HasOutStanding  bool
-    MaxRetries      int
-    Queues          []string
-    // Port     int
-    // Username string
-    // Password string
+	Name           string
+	SessionType    string
+	SMSC           string
+	SystemID       string
+	Password       string
+	SystemType     string
+	MaxOutStanding int
+	HasOutStanding bool
+	MaxRetries     int
+	Queues         []string
 }
 
+var config *Config
+
 func LoadConfig() *Config {
-    if config != nil{
-        return config
-    }
+	if config != nil {
+		return config
+	}
 
-    config = &Config{
-        RedisURL:         os.Getenv("REDIS_URL"),
-        RateLimit:        100,
-        SMPPConfig:       loadSMPPConfig(),
-        DatabaseConfig:   loadDatabaseConfig(),
-        ProvidersConfig:  loadProvidersConfig(),
-    }
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // required if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	viper.AutomaticEnv()          // read in environment variables that match
 
-    return config
+	// Set default values
+	setDefaults()
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Printf("Error reading config file, %s", err)
+	}
+
+	config = &Config{
+		RedisURL:        viper.GetString("redis_url"),
+		RateLimit:       viper.GetInt("rate_limit"),
+		SMPPConfig:      loadSMPPConfig(),
+		DatabaseConfig:  loadDatabaseConfig(),
+		ProvidersConfig: loadProvidersConfig(),
+	}
+
+	return config
+}
+
+func setDefaults() {
+	viper.SetDefault("rate_limit", 100)
+	viper.SetDefault("database_config.port", 3306)
+	viper.SetDefault("database_config.host", "localhost")
+	viper.SetDefault("database_config.user", "root")
+	viper.SetDefault("database_config.password", "password")
+	viper.SetDefault("database_config.dbname", "smpp_db")
 }
 
 func loadSMPPConfig() SMPPConfig {
-    host := os.Getenv("SMPP_HOST")
-    port := os.Getenv("SMPP_PORT")
-    return SMPPConfig{
-        SMSC:       host + ":" + port,
-        SystemID:   os.Getenv("SYSTEM_ID"),
-        Password:   os.Getenv("PASSWORD"),
-        SystemType: os.Getenv("SYSTEM_TYPE"),
-    }
+	return SMPPConfig{
+		SMSC:       viper.GetString("smpp_config.smsc"),
+		SystemID:   viper.GetString("smpp_config.system_id"),
+		Password:   viper.GetString("smpp_config.password"),
+		SystemType: viper.GetString("smpp_config.system_type"),
+	}
 }
 
 func loadDatabaseConfig() DatabaseConfig {
-    return DatabaseConfig{
-        Host:     os.Getenv("DB_HOST"),
-        Port:     3306,
-        User:     os.Getenv("DB_USER"),
-        Password: os.Getenv("DB_PASSWORD"),
-        DBName:   os.Getenv("DB_NAME"),
-    }
+	return DatabaseConfig{
+		Host:     viper.GetString("database_config.host"),
+		Port:     viper.GetInt("database_config.port"),
+		User:     viper.GetString("database_config.user"),
+		Password: viper.GetString("database_config.password"),
+		DBName:   viper.GetString("database_config.dbname"),
+	}
 }
 
 func loadProvidersConfig() []Provider {
-    return []Provider{
-        loadProviderConfig("PROVIDER_A"),
-        loadProviderConfig("PROVIDER_B"),
-        loadProviderConfig("PROVIDER_C"),
-    }
-}
-
-
-func loadProviderConfig(provider string) Provider {
-    host := strings.TrimSpace(os.Getenv(provider + "_HOST"))
-    port := strings.TrimSpace(os.Getenv(provider + "_PORT"))
-    smsc := host + ":" + port
-    sessionType := strings.TrimSpace(os.Getenv(provider + "_SESSION_TYPE"))
-    name := strings.TrimSpace(os.Getenv(provider + "_NAME"))
-    systemID := strings.TrimSpace(os.Getenv(provider + "_SYSTEM_ID"))
-    password := strings.TrimSpace(os.Getenv(provider + "_PASSWORD"))
-    systemType := strings.TrimSpace(os.Getenv(provider + "_SYSTEM_TYPE"))
-
-    if name == "" || smsc == "" || systemID == "" || password == "" {
-        return Provider{}
-    }
-
-    maxOutstanding, err := strconv.Atoi(strings.TrimSpace(os.Getenv(provider + "_MAX_OUTSTANDING")))
-    globalMaxOutstanding := 100    
-    if err != nil {
-        maxOutstanding = globalMaxOutstanding
-    }
-
-    maxRetries, err := strconv.Atoi(strings.TrimSpace(os.Getenv(provider + "_MAX_RETRIES")))
-    globalMaxRetries := 3
-    if err != nil {
-        maxRetries = globalMaxRetries
-    }
-
-    hasOutStanding, err := strconv.ParseBool(strings.TrimSpace(os.Getenv(provider + "_HAS_OUTSTANDING_LIMIT")))
-    globalOutStanding := false
-    if err != nil {
-        hasOutStanding = globalOutStanding
-    }
-
-    return Provider{
-        Name:           name,
-        SessionType:    sessionType,
-        SMSC:           smsc,
-        SystemID:       systemID,
-        Password:       password,
-        SystemType:     systemType,
-        MaxOutStanding: maxOutstanding,
-        HasOutStanding: hasOutStanding,
-        MaxRetries:     maxRetries,
-        Queues:         loadQueues(provider),
-    }
-}
-
-func loadQueues(provider string) []string {
-    return strings.Split(os.Getenv(provider + "_QUEUES"), ",")
-}
-
-func GetProviderCfg(name string) Provider{
-    for _, provider := range providersList{
-        if provider.Name == name {
-            return provider
-        }
-    }
-    return Provider{}
+	providers := []Provider{}
+	if err := viper.UnmarshalKey("providers", &providers); err != nil {
+		log.Printf("Error unmarshaling providers config, %s", err)
+	}
+	return providers
 }
