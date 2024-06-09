269d375fc1346e5e49bfb2bf7d023de8ebe7d56f
Amr M. Elsayed <rixtrayker@github.com>
2024-06-09 18:03:57 +0300

    refactor: Update .gitignore to include config.yaml

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@ module github.com/rixtrayker/demo-smpp
 go 1.22.1
 
 require (
-	github.com/AppsFlyer/go-sundheit v0.5.0
-	github.com/cloudflare/tableflip v1.2.3
 	github.com/joho/godotenv v1.5.1
+	github.com/knadh/koanf/parsers/yaml v0.1.0
+	github.com/knadh/koanf/providers/env v0.1.0
+	github.com/knadh/koanf/providers/file v0.1.0
+	github.com/knadh/koanf/v2 v2.1.1
 	github.com/linxGnu/gosmpp v0.2.1
 	github.com/redis/go-redis/v9 v9.5.1
 	github.com/sirupsen/logrus v1.9.3
-	github.com/ysmood/gotrace v0.6.0
 	go.uber.org/atomic v1.11.0
+	go.uber.org/goleak v1.3.0
 	golang.org/x/time v0.5.0
 	golang.org/x/vuln v1.1.1
 	gorm.io/driver/mysql v1.5.6
@@ -20,31 +22,23 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/sagikazarmark/locafero v0.4.0 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.11.0 // indirect
-	github.com/spf13/cast v1.6.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.19.0 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	golang.org/x/mod v0.17.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/tools v0.21.1-0.20240514024235-59d9797072e7 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -4,114 +4,84 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/spf13/viper"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+	"github.com/sirupsen/logrus"
 )
 
 type Config struct {
-	RedisURL        string
-	RateLimit       int
-	SMPPConfig      SMPPConfig
-	DatabaseConfig  DatabaseConfig
-	ProvidersConfig []Provider
+	RedisURL        string        `koanf:"redis_url"`
+	RateLimit       int           `koanf:"rate_limit"`
+	SMPPConfig      SMPPConfig    `koanf:"smpp_config"`
+	DatabaseConfig  DatabaseConfig `koanf:"database_config"`
+	ProvidersConfig []Provider    `koanf:"providers"`
 }
 
 type SMPPConfig struct {
-	SMSC       string
-	SystemID   string
-	Password   string
-	SystemType string
+	SMSC       string `koanf:"smsc"`
+	SystemID   string `koanf:"system_id"`
+	Password   string `koanf:"password"`
+	SystemType string `koanf:"system_type"`
 }
 
 type DatabaseConfig struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
-	DBName   string
+	Host     string `koanf:"host"`
+	Port     int    `koanf:"port"`
+	User     string `koanf:"user"`
+	Password string `koanf:"password"`
+	DBName   string `koanf:"dbname"`
 }
 
 type Provider struct {
-	Name           string
-	SessionType    string
-	SMSC           string
-	Address 	   string 
-	Port 		   int
-	SystemID       string
-	Password       string
-	SystemType     string
-	MaxOutStanding int
-	HasOutStanding bool
-	MaxRetries     int
-	Queues         []string
+	Name           string   `koanf:"name"`
+	SessionType    string   `koanf:"session_type"`
+	SMSC           string   `koanf:"smc"` // This will be derived from Address and Port
+	Address        string   `koanf:"address"`
+	Port           int      `koanf:"port"`
+	SystemID       string   `koanf:"system_id"`
+	Password       string   `koanf:"password"`
+	SystemType     string   `koanf:"system_type"`
+	MaxOutStanding int      `koanf:"max_outstanding"`
+	HasOutStanding bool     `koanf:"has_outstanding"`
+	MaxRetries     int      `koanf:"max_retries"`
+	Queues         []string `koanf:"queues"`
 }
 
 var config *Config
+var k = koanf.New(".")
 
 func LoadConfig() *Config {
 	if config != nil {
 		return config
 	}
 
-	viper.SetConfigName("config") // name of config file (without extension)
-	viper.SetConfigType("yaml")   // required if the config file does not have the extension in the name
-	viper.AddConfigPath(".")      // optionally look for config in the working directory
-	viper.AutomaticEnv()          // read in environment variables that match
-
-	// Set default values
-	setDefaults()
-
-	if err := viper.ReadInConfig(); err != nil {
-		log.Printf("Error reading config file, %s", err)
+	if err := k.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		log.Fatalf("Error loading config: %v", err)
 	}
 
-	config = &Config{
-		RedisURL:        viper.GetString("redis_url"),
-		RateLimit:       viper.GetInt("rate_limit"),
-		SMPPConfig:      loadSMPPConfig(),
-		DatabaseConfig:  loadDatabaseConfig(),
-		ProvidersConfig: loadProvidersConfig(),
+	config = &Config{}
+	if err := k.Unmarshal("", config); err != nil {
+		log.Fatalf("Error unmarshaling config: %v", err)
+	}
+
+	// Derive SMSC field in providers
+	for i := range config.ProvidersConfig {
+		config.ProvidersConfig[i].SMSC = fmt.Sprintf("%s:%d", config.ProvidersConfig[i].Address, config.ProvidersConfig[i].Port)
+		logProviderConfig(config.ProvidersConfig[i])
 	}
 
 	return config
 }
 
-func setDefaults() {
-	viper.SetDefault("rate_limit", 100)
-	viper.SetDefault("database_config.port", 3306)
-	viper.SetDefault("database_config.host", "localhost")
-	viper.SetDefault("database_config.user", "root")
-	viper.SetDefault("database_config.password", "")
-	viper.SetDefault("database_config.dbname", "go_client")
-}
-
-func loadSMPPConfig() SMPPConfig {
-	return SMPPConfig{
-		SMSC:       viper.GetString("smpp_config.smsc"),
-		SystemID:   viper.GetString("smpp_config.system_id"),
-		Password:   viper.GetString("smpp_config.password"),
-		SystemType: viper.GetString("smpp_config.system_type"),
-	}
-}
-
-func loadDatabaseConfig() DatabaseConfig {
-	return DatabaseConfig{
-		Host:     viper.GetString("database_config.host"),
-		Port:     viper.GetInt("database_config.port"),
-		User:     viper.GetString("database_config.user"),
-		Password: viper.GetString("database_config.password"),
-		DBName:   viper.GetString("database_config.dbname"),
-	}
-}
-
-func loadProvidersConfig() []Provider {
-	providers := []Provider{}
-	if err := viper.UnmarshalKey("providers", &providers); err != nil {
-		log.Printf("Error unmarshaling providers config, %s", err)
-	}
-	
-	for _, p := range providers {
-		p.SMSC = p.Address + ":" + fmt.Sprintf("%d", p.Port)
-	}
-
-	return providers
+func logProviderConfig(provider Provider) {
+	logrus.WithFields(logrus.Fields{
+		"Name":           provider.Name,
+		"SessionType":    provider.SessionType,
+		"SMSC":           provider.SMSC,
+		"SystemID":       provider.SystemID,
+		"Password":       provider.Password,
+		"SystemType":     provider.SystemType,
+		"MaxOutStanding": provider.MaxOutStanding,
+	}).Info("Provider")
 }
