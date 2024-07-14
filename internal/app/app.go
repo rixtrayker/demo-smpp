fe35350cf5ed93cb362745295875caec08c9408a
Amr M. Elsayed <rixtrayker@github.com>
2024-07-15 02:28:19 +0300

    refactor: Add error logging for empty ProvidersConfig and Provider
    Improve error handling in the `initClients` function by adding error logging when `ProvidersConfig` is empty or a provider is empty. This ensures that any issues with the configuration are properly logged, making it easier to identify and troubleshoot problems.

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 
 	"github.com/phuslu/log"
+	"github.com/sirupsen/logrus"
 
 	"github.com/rixtrayker/demo-smpp/internal/config"
 	clients "github.com/rixtrayker/demo-smpp/internal/gateway"
@@ -36,12 +37,14 @@ func initClients(ctx context.Context, cfg *config.Config) {
 
     if len(cfg.ProvidersConfig) == 0 {
         logger.Error().Msg("ProvidersConfig is empty")
+        logrus.Error("ProvidersConfig is empty")
         return
     }
 
     for _, provider := range cfg.ProvidersConfig {
         if reflect.DeepEqual(provider, config.Provider{}) {
             logger.Error().Msg("Provider " + provider.Name + " is empty")
+            logrus.Error("Provider " + provider.Name + " is empty")
             continue
         }
         wg.Add(1)
@@ -57,6 +60,7 @@ func initClients(ctx context.Context, cfg *config.Config) {
 
             if err != nil {
                 logger.Error().Err(err).Msg("Failed to create client for provider " + provider.Name)
+                logrus.Error("Failed to create client for provider " + provider.Name)
                 return
             }
 
@@ -81,8 +85,10 @@ func Start(ctx context.Context, cfg *config.Config) {
 func handleShutdown(ctx context.Context) {
     <-ctx.Done()
     logger.Info().Msg("Context canceled, initiating shutdown...")
+    logrus.Info("Context canceled, initiating shutdown...")
     for _, c := range appClients {
         c.Stop()
     }
     logger.Info().Msg("All sessions stopped.")
+    logrus.Info("All sessions stopped.")
 }
